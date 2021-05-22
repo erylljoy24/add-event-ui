225b47dd3d27f1c2d8c9529ce124400f402d0f4f
erylljoy24 <eryll.salamanes24@gmail.com>
2021-05-23 01:47:50 +0800

    Minor edits

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,56 +3,59 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4">
+        
         <div>
-          <label for="calendar" class="form-label">Event</label>
-          <div>
-            <input id="calendar" type="text" class="input-group">
-          </div>
-          <div class="row form-margin">
-            <div class="col-md-6">
-              <label for="start-date" class="form-label">From</label>
-              <div>
-                <input id="start-date" type="date" class="input-group" v-model="startDate">
+          <form v-on:submit.prevent="addEvent">
+            <label for="calendar" class="form-label">Event</label>
+            <div>
+              <input id="calendar" type="text" class="input-group" v-model="form.title">
+            </div>
+            <div class="row form-margin">
+              <div class="col-md-6">
+                <label for="start-date" class="form-label">From</label>
+                <div>
+                  <input id="start-date" type="date" class="input-group" v-model="form.start">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <label for="end-date" class="form-label">To</label>
+                <div>
+                  <input id="end-date" type="date" class="input-group" v-model="form.end">
+                </div>
               </div>
             </div>
-            <div class="col-md-6">
-              <label for="end-date" class="form-label">To</label>
-              <div>
-                <input id="end-date" type="date" class="input-group" v-model="endDate">
-              </div>
+            <div class="form-margin">
+              <label class="checkbox-inline">
+                <input type="checkbox" id="mon" v-model="form.days_selected" :value="1">Mon
+              </label>
+              <label class="checkbox-inline">
+                <input type="checkbox" id="tue" v-model="form.days_selected" :value="2">Tue
+              </label>
+              <label class="checkbox-inline">
+                <input type="checkbox" id="wed" v-model="form.days_selected" :value="3">Wed
+              </label>
+              <label class="checkbox-inline">
+                <input type="checkbox" id="thu" v-model="form.days_selected" :value="4">Thu
+              </label>
+              <label class="checkbox-inline">
+                <input type="checkbox" id="fri" v-model="form.days_selected" :value="5">Fri
+              </label>
+              <label class="checkbox-inline">
+                <input type="checkbox" id="sat" v-model="form.days_selected" :value="6">Sat
+              </label>
+              <label class="checkbox-inline">
+                <input type="checkbox" id="sun" v-model="form.days_selected" :value="7">Sun
+              </label>
             </div>
-          </div>
-          <div class="form-margin">
-            <label class="checkbox-inline">
-              <input type="checkbox" id="mon" value="1">Mon
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="tue" value="2">Tue
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="wed" value="3">Wed
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="thu" value="4">Thu
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="fri" value="5">Fri
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="sat" value="6">Sat
-            </label>
-            <label class="checkbox-inline">
-              <input type="checkbox" id="sun" value="7">Sun
-            </label>
-          </div>
-          <button type="submit" class="btn btn-success form-margin">Save</button>
+            <button type="submit" class="btn btn-success form-margin">Save</button>
+          </form>
         </div>
       </div>
       <div class="col-md-8">
         <calendar
-                :eventCategories="eventCategories"
-                :events="events"
-                ref="calendar"
+          :eventCategories="eventCategories"
+          :events="events"
+          ref="calendar"
         />
       </div>
     </div>
@@ -63,6 +66,8 @@
     // import Datepicker from 'vuejs-datepicker';
     import { Calendar } from 'vue-sweet-calendar'
     import 'vue-sweet-calendar/dist/SweetCalendar.css'
+    import axios from 'axios';
+
     export default {
       name: 'HelloWorld',
       props: {
@@ -75,56 +80,61 @@
       },
       data() {
           return {
-              calendarData: {},
               eventCategories: [
                   {
                       id: 1,
                       title: 'Personal',
                       textColor: 'white',
                       backgroundColor: 'Blue'
-                  },
-                  {
-                      id: 2,
-                      title: 'Company-wide',
-                      textColor: 'white',
-                      backgroundColor: 'red'
                   }
               ],
               events: [
-                  {
-                      title: 'Event 1',
-                      start: '2019-04-02',
-                      end: '2019-04-04',
-                      repeat: 'monthly',
-                      categoryId: 1
-                  },
-                  {
-                      title: 'Event 2',
-                      start: '2019-04-08',
-                      end: '2019-04-09',
-                      repeat: 'yearly',
-                      categoryId: 1
-                  },
-                  {
-                      title: 'Event 3',
-                      start: '2019-04-10',
-                      end: '2019-04-11',
-                      repeat: 'never',
-                      categoryId: 2
-                  },
-                  {
-                      title: 'Event 4',
-                      start: '2019-04-23',
-                      end: '2019-04-23',
-                      repeat: 'monthly',
-                      categoryId: 2
-                  }
               ],
+
+
+              form: {
+                title: '',
+                start: '',
+                end: '',
+                days_selected: []
+              }
           }
+      },
+      mounted () {
+        axios
+        .get('http://127.0.0.1:8000/api/events/all')
+        .then((response) => {
+          this.events = response.data.events.map((item) => {
+            return {
+              title: item.title,
+              start: item.start,
+              end: item.end,
+              categoryId: item.categoryId
+            }
+          })
+        })
       },
       methods: {
           goToday() {
               this.$refs.calendar.goToday()
+          },
+
+          addEvent(){
+            console.log(this.form)
+            axios.post('http://127.0.0.1:8000/api/event/add', this.form)
+              .then((response) => {
+                this.events = response.data.events.map((item) => {
+                  return {
+                    title: item.title,
+                    start: item.start,
+                    end: item.end,
+                    categoryId: item.categoryId
+                  }
+                })
+              })
+              .catch(error => {
+                console.log(error)
+              })
           }
       }
     }
